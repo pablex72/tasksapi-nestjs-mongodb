0b4a5864c2663f1ca3f8bc10896e9472f883bd22
pablo <tonyp2hoy@gmail.com>
2024-06-05 19:18:13 -0400

    feat: moongose and service config

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from '../schemas/task.schema';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    constructor(@InjectModel(Task.name) private taskModel: Model<Task>){
+    }
+
+    findAll() {
+        this.taskModel.find();
+
+    }
+    async create(createTask:any){
+        const newTask = new this.taskModel(createTask);
+        return newTask.save();
+    }
+
+    async findOnde(id: string){
+        return this.taskModel.findById(id)
+    }
+
+    async delete(id:string){
+        return this.taskModel.findByIdAndDelete(id)
+    }
+
+    async update(id:string, task:any){
+        return this.taskModel.findByIdAndUpdate(id,task)
+    }
+}
