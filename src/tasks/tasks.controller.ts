77e22b4dba26160a8a6bebb0e06767bcf66917a8
pablo <tonyp2hoy@gmail.com>
2024-06-07 23:59:20 -0400

    feat: adding controller v1 and pipes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    constructor(private tasksService: TasksService){}
+
+    // creating routes get post, delete
+    @Get()
+    findAll(){
+        return this.tasksService.findAll();
+    }
+    
+    // /tasks/100
+    @Get(':id')
+    findOne(){
+        return 'Get one Task'
+    }
+
+    @Post()
+    create(@Body() body: CreateTaskDto){
+        return this.tasksService.create(body)
+    }
+    
+    @Put()
+    update(){
+        return 'update task'
+    }
+
+}
